e68944899de20a9d07e7fac2bc8ce657353151e0
SI Xiaolong <sixiaolonggithub@gmail.com>
2025-01-23 15:16:14 +0800

    更新README。 VideoBackground组件化。

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,10 @@ import Footer from './components/Footer.vue'
 import Head from './components/Head.vue'
 import ShareButton from './components/ShareButton.vue'
 import Email from './components/Email.vue'
+import VideoBackground from './components/VideoBackground.vue'
 </script>
 
 <template>
-  <video class="video-background" autoplay loop muted playsinline>
-    <source
-      src="https://cdn.fastly.steamstatic.com/steamcommunity/public/images/items/2861720/85882f90ad8ca69467610883a9fa95ecc2f071ab.webm"
-      type="video/webm" />
-    Your browser does not support the video tag.
-  </video>
 
 
   <ShareButton Msg="司晓龙的 Link Collections" />
@@ -45,6 +40,7 @@ import Email from './components/Email.vue'
   <br>
   <br>
   <Footer />
+  <VideoBackground />
 </template>
 
 <style scoped>
@@ -54,16 +50,5 @@ import Email from './components/Email.vue'
   align-items: center;
   min-height: 100vh;
   gap: 20px;
-}
-
-.video-background {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  z-index: -1;
-  opacity: 0.2;
 }
 </style>
---
+++ src/components/VideoBackground.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <video class="video-background" autoplay loop muted playsinline>
+        <source
+            src="https://cdn.fastly.steamstatic.com/steamcommunity/public/images/items/2861720/85882f90ad8ca69467610883a9fa95ecc2f071ab.webm"
+            type="video/webm" />
+        Your browser does not support the video tag.
+    </video>
+</template>
+
+<style scoped>
+.video-background {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1;
+    opacity: 0.2;
+}
+</style>
